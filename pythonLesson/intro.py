10f8831030070e3d0b8693044b73689c06706a93
BatWoman01 <55641388+BatWoman01@users.noreply.github.com>
2023-06-09 15:45:26 -0700

    pythonLesson

--- pythonLesson/intro.py
+++ pythonLesson/intro.py
@@ -1,4 +1,23 @@
-# Print Message
+# Print Messages and Outputs
 
 print("Hello, World!")
+print(50)
+print(1000)
+print(3.142)
 
+# Print Multiple Statements
+
+print(50, 1000, 3.142, "Hello, WOrld!")
+
+print("Hello", end="")
+print("World!")
+
+print("Hello", end=" ")
+print("World")
+
+# Comments vs Docstrings
+
+# Comments involve the line starting with '#' 
+
+""" Docustrings start and end with triple quotes as seen with 
+    thins line """
