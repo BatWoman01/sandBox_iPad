6eda3947e304b6e293668317fa285c25431ac45d
BatWoman01 <55641388+BatWoman01@users.noreply.github.com>
2023-06-09 15:52:27 -0700

    pythonLesson

---
+++ pythonLesson/dataTypesAndVariables.py
@@ -0,0 +1,10 @@
+# Data Types are defined as the type and range of values that it 
+# can have.
+
+# Numbers 
+#   1, 2, 3, 4, 5, .....
+# Strings
+#   "Hello, World", "VR is interesting gameplay"
+# Booleans
+#   True or False
+
